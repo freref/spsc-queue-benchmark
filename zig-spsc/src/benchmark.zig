2c378b3a5c36bc92433561021e072326bbd131e5
freref <35976402+freref@users.noreply.github.com>
2025-09-08 16:05:32 +0200

    add: freref-po2

--- zig-spsc/src/benchmark.zig
+++ zig-spsc/src/benchmark.zig
@@ -5,7 +5,18 @@ const spsc_ring = @import("spsc_ring");
 const total_rounds: u64 = 10_000_000;
 const capacity: usize = 16_777_216; // This is power of two which is needed for cdolan implementation
 
-fn spscReadWorker(q: *spsc_queue.SpscQueue(i32), rounds: u64) void {
+fn spscReadWorker(q: *spsc_queue.SpscQueue(i32, false), rounds: u64) void {
+    var i: u64 = 0;
+    while (i < rounds) {
+        while (q.front() == null) {}
+        const val = q.front().?.*;
+        if (val != i) @panic("out of order");
+        q.pop();
+        i += 1;
+    }
+}
+
+fn spscPo2ReadWorker(q: *spsc_queue.SpscQueue(i32, true), rounds: u64) void {
     var i: u64 = 0;
     while (i < rounds) {
         while (q.front() == null) {}
@@ -31,7 +42,29 @@ fn spscRingReadWorker(r: *spsc_ring.Ring(i32), rounds: u64) void {
 
 pub fn main() !void {
     {
-        var queue = try spsc_queue.SpscQueue(i32).initCapacity(std.heap.page_allocator, capacity);
+        var queue = try spsc_queue.SpscQueue(i32, true).initCapacity(std.heap.page_allocator, capacity);
+        defer queue.deinit();
+
+        var reader = try std.Thread.spawn(.{}, spscPo2ReadWorker, .{ &queue, total_rounds });
+
+        const start_ns_q: i128 = std.time.nanoTimestamp();
+
+        var i_q: i32 = 0;
+        while (i_q < total_rounds) : (i_q += 1) {
+            queue.push(i_q);
+        }
+
+        reader.join();
+
+        const end_ns_q: i128 = std.time.nanoTimestamp();
+        const elapsed_ns_q: u128 = @intCast(end_ns_q - start_ns_q);
+        const ops_per_ms_q: u128 = (@as(u128, total_rounds) * 1_000_000) / elapsed_ns_q;
+
+        std.debug.print("freref-po2:\n {d} ops/ms\n", .{ops_per_ms_q});
+    }
+
+    {
+        var queue = try spsc_queue.SpscQueue(i32, false).initCapacity(std.heap.page_allocator, capacity);
         defer queue.deinit();
 
         var reader = try std.Thread.spawn(.{}, spscReadWorker, .{ &queue, total_rounds });
